1cea02b2ca6863a300bea2e390902e870dccd143
Nitin Dhaware <nitin@techvision.net.in>
2013-03-18 16:42:51 +0530

    Added whole app in the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BhaktaSanwad::Application.config.secret_token = 'adf2541cdbd0868476db1383ff084c81e3d79676962744ab1db970e387797148bcd0d84371d7bd7befeb5f81c65e5d0cf55f86532681607e4f46018abb2ed8c2'
